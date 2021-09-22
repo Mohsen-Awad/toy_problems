73150dddef5401a65d02adfcbb46573fdd0276af
Mohsen Awad <mawad@voice.com>
2021-09-22 11:31:33 +0300

    getUniqueValues

--- unique/index.test.js
+++ unique/index.test.js
@@ -10,13 +10,31 @@
  */
 
 /**
-  * example usage:
-  * const arrOfNum = [1, 2, 2, 4, 5, 6, 6];
-  * console.log(getUniqueValues(arrOfNum)); // [1, 2, 4, 5, 6]
-  */
-const arrOfNum = [1, 2, 2, 4, 5, 6, 6];
+ * example usage:
+ * const arrOfNum = [1, 2, 2, 4, 5, 6, 6];
+ * console.log(getUniqueValues(arrOfNum)); // [1, 2, 4, 5, 6]
+ */
+const arrOfNum = [1, 2, 2, 4, 5, 6, 6]
 
 const getUniqueValues = (arrOfNum) => {
   // TO DO
+
+  const newSet = new Set()
+  for (i in arrOfNum) newSet.add(arrOfNum[i])
+
+  return [...newSet]
+
+  // const newObj = {}
+  // for (i in arrOfNum) newObj[arrOfNum[i]] = arrOfNum[i]
+
+  // return Object.values(newObj)
 }
-console.log(getUniqueValues(arrOfNum)); // [1, 2, 4, 5, 6]
+console.log(getUniqueValues(arrOfNum)) // [1, 2, 4, 5, 6]
+
+describe('Tests', () => {
+  it('test getUniqueValues', () => {
+    expect(getUniqueValues(arrOfNum)).toEqual([1, 2, 4, 5, 6])
+    expect(getUniqueValues([2, 2, 2, 2, 2, 2])).toEqual([2])
+    expect(getUniqueValues([1, 2, 3, 4, 1, 2, 3, 4])).toEqual([1, 2, 3, 4])
+  })
+})
