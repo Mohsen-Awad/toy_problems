869a155aea08360cda5cc91d6141418151daa4e0
Mohsen Awad <mawad@voice.com>
2021-09-28 12:11:41 +0300

    translateRomanNumeral

--- oldRomanTranslate/index.test.js
+++ oldRomanTranslate/index.test.js
@@ -1,4 +1,3 @@
-
 /**
  * Given a roman numeral as input, write a function that converts the roman
  * numeral to a number and outputs it.
@@ -23,9 +22,34 @@ const DIGIT_VALUES = {
   L: 50,
   C: 100,
   D: 500,
-  M: 1000
-};
+  M: 1000,
+}
 
- const translateRomanNumeral = (romanNumeral)=> {
-   // TO DO ...
+const translateRomanNumeral = (romanNumeral) => {
+  // TO DO ...
+  let sum = DIGIT_VALUES[romanNumeral[0]]
+  for (i = 1; i < romanNumeral.length; i++) {
+    if (DIGIT_VALUES[romanNumeral[i]] > DIGIT_VALUES[romanNumeral[i - 1]])
+      sum =
+        sum -
+        DIGIT_VALUES[romanNumeral[i - 1]] +
+        DIGIT_VALUES[romanNumeral[i]] -
+        DIGIT_VALUES[romanNumeral[i - 1]]
+    else sum += DIGIT_VALUES[romanNumeral[i]]
   }
+
+  return sum
+}
+
+describe('Tests', () => {
+  it('translateRomanNumeral', () => {
+    expect(translateRomanNumeral('IV')).toEqual(4)
+    expect(translateRomanNumeral('XXI')).toEqual(21)
+    expect(translateRomanNumeral('XX')).toEqual(20)
+    expect(translateRomanNumeral('XVI')).toEqual(16)
+    expect(translateRomanNumeral('XXIV')).toEqual(24)
+    expect(translateRomanNumeral('XIII')).toEqual(13)
+    expect(translateRomanNumeral('XIV')).toEqual(14)
+    expect(translateRomanNumeral('XXVII')).toEqual(27)
+  })
+})
