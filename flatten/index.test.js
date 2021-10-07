a5f5cee377556a4199a38d62e7b850a1d1102525
Mohsen Awad <mawad@voice.com>
2021-10-07 13:06:31 +0300

    flatten

--- flatten/index.test.js
+++ flatten/index.test.js
@@ -15,6 +15,37 @@ flatten('a', ['b', 2], 3, null, [[4], ['c']])
 // returns ['a', 'b', 2, 3, null, 4, 'c']
 */
 
-const flatten = () => {
+const flatten = function () {
+  let arr = []
+  for (i in flatten.arguments) {
+    if (flatten.arguments[i] && typeof flatten.arguments[i] === 'object') {
+      arr = arr.concat(flatten(...flatten.arguments[i]))
+    } else {
+      arr.push(flatten.arguments[i])
+    }
+  }
+  return arr
+}
 
-};
+describe('Tests', () => {
+  it('test flatten', () => {
+    expect(flatten('a', 3, 'c', null, [5, 7])).toEqual([
+      'a',
+      3,
+      'c',
+      null,
+      5,
+      7,
+    ])
+    expect(flatten('a', ['b', 2], 3, null, [[4], ['c']])).toEqual([
+      'a',
+      'b',
+      2,
+      3,
+      null,
+      4,
+      'c',
+    ])
+    expect(flatten(1, [2, 3], 4, 5, [6, [7]])).toEqual([1, 2, 3, 4, 5, 6, 7])
+  })
+})
