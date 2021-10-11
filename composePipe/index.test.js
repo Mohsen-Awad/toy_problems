8a0507e04ca6f6da163eebab1fd4eea59f4f439e
Mohsen Awad <mawad@voice.com>
2021-10-11 13:17:55 +0300

    composePipe

--- composePipe/index.test.js
+++ composePipe/index.test.js
@@ -32,3 +32,71 @@
   var addAndMultiplyTwice = pipe(add2, multiplyBy3, multiplyBy3);
   addAndMultiplyTwice(5); //should be 63
  */
+
+// *** Compose ***
+const greet = function (name) {
+  return 'hello ' + name
+}
+
+const exclaim = function (statement) {
+  return statement.toUpperCase() + '!'
+}
+
+/**
+ *
+ * @param  {...any} args
+ * @returns {function}
+ */
+const compose = function (...args) {
+  return (num) => {
+    for (i = args.length - 1; i >= 0; i--) num = args[i](num)
+    return num
+  }
+}
+// const compose = (x, y) => (z) => x(y(z))
+
+const welcome = compose(exclaim, greet)
+const welcomeTwice = compose(exclaim, greet, greet)
+
+// *** Pipe ***
+const add2 = function (number) {
+  return number + 2
+}
+
+const multiplyBy3 = function (number) {
+  return number * 3
+}
+
+/**
+ *
+ * @param  {...any} args
+ * @returns {function}
+ */
+const pipe = function (...args) {
+  return (num) => {
+    for (i = 0; i <= args.length - 1; i++) num = args[i](num)
+    return num
+  }
+}
+// const pipe = (x, y) => (z) => y(x(z))
+
+const addAndMultiply = pipe(add2, multiplyBy3)
+const addAndMultiplyTwice = pipe(add2, multiplyBy3, multiplyBy3)
+
+describe('test', () => {
+  it('Compose function', () => {
+    expect(welcome('phillip')).toEqual('HELLO PHILLIP!')
+  })
+
+  it('Compose function', () => {
+    expect(welcomeTwice('Mohsen')).toEqual('HELLO HELLO MOHSEN!')
+  })
+
+  it('Pipe function', () => {
+    expect(addAndMultiply(5)).toEqual(21)
+  })
+
+  it('Pipe function', () => {
+    expect(addAndMultiplyTwice(5)).toEqual(63)
+  })
+})
